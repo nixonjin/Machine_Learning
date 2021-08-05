2617a05db1c2685cd2401c62aa007585a63ddcb6
nixonjin <2465004358@qq.com>
2021-08-05 19:04:37 +0800

    update 2021/8/5

--- deep_learning/recommeder_system/seq_aware_recommend.py
+++ deep_learning/recommeder_system/seq_aware_recommend.py
@@ -1,21 +1,30 @@
 #%%
 import random
-import mxnet as mx
-from mxnet import gluon, np, npx
-from mxnet.gluon import nn
-from d2l import mxnet as d2l
+import torch
+from torch import nn 
 
-npx.set_np()
 
 #%%
-class Caser(nn.Block):
-    def __init__(self, num_factors, num_users, num_items, L=5,d=16,
-                d_prime=4, drop_ratio=0.05,**kwargs):
-        super(Caser, self).__init__(**kwargs)
-    
+class Caser(nn.Module):
+    def __init__(self, num_factors, num_users, num_items, L=5, d=16,
+                 d_prime=4):
+        self.P = nn.Embedding(num_users, num_factors)
+        self.Q = nn.Embedding(num_items, num_factors)
+        self.
 
 
-    def forward(self, user_id, seq, item_id):
+    def forward(self,user_id,item_id,seq):
+        pass
+
 #%%
-import torch 
+num_users=10
+num_items=10
+P=nn.Embedding(num_users,32)
+Q=nn.Embedding(num_items,32)
+#%%
+seq = torch.as_tensor([1,2,3,4,5,6])
+temp = Q(seq)
+# %%
+temp = temp.unsqueeze(1)
+temp.shape
 # %%
